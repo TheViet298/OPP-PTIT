91dcdadf0cd31890090c4748ad8e197440c9f218
TheViet-IT <140674386+TheViet298@users.noreply.github.com>
2024-01-02 09:46:52 +0700

    J03021 - ĐIỆN THOẠI CỤC GẠCH.java

--- J03021 - ĐIỆN THOẠI CỤC GẠCH.java
+++ J03021 - ĐIỆN THOẠI CỤC GẠCH.java
@@ -1,39 +1,35 @@
-//test còn sai
-import java.util.*;
+import java.util.Scanner;
+import java.math.BigInteger;
 
-public class J03021 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        int t = input.nextInt();
-        input.nextLine(); 
-        while (t-- > 0) {
-            String s = input.next();
-            if (isPalindromeNumber(s)) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
-            }
-        }
-        input.close();
+public class Main{
+    static char convert(char x){
+        if("ABC".indexOf(x) > -1) return '2';
+        if("DEF".indexOf(x) > -1) return '3';
+        if("GHI".indexOf(x) > -1) return '4';
+        if("JKL".indexOf(x) > -1) return '5';
+        if("MNO".indexOf(x) > -1) return '6';
+        if("PQRS".indexOf(x) > -1) return '7';
+        if("TUV".indexOf(x) > -1) return '8';
+        return '9';
     }
-
-    private static boolean isPalindromeNumber(String s) {
-        String[] keypad = {
-            "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
-        };
-        
-        StringBuilder translated = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            for (int j = 0; j < keypad.length; j++) {
-                if (keypad[j].contains(String.valueOf(c))) { // check có xuất hiện tương ứng với keypad hay không
-                    translated.append(j + 2); 
-                    break;
-                }
+    static boolean dx(char[] a, int n){
+        for(int i=0;i<=n/2;i++){
+            if(a[i]!=a[n-i-1]) return false;
+        }
+        return true;
+    }
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            String s = sc.next();
+            s = s.toUpperCase();
+            char[] c = new char[s.length()];
+            for(int i=0; i<s.length(); i++){
+                c[i] = convert(s.charAt(i));
             }
-        } 
-        
-        return translated.toString().equals(translated.reverse().toString()); // chuyển chuỗi check thuận nghịch
+            if(dx(c, c.length)) System.out.println("YES");
+            else System.out.println("NO");
+        }
     }
 }
